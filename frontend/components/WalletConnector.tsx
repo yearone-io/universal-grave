d24ef1cae6699b4f0bdcd41f4d2c3933d85908c0
Ledespana <no-reply@ledespana.com>
2023-11-09 10:56:07 +0100

    most basic connect

--- frontend/components/WalletConnector.tsx
+++ frontend/components/WalletConnector.tsx
@@ -1,96 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import Onboard from '@web3-onboard/core';
-import luksoModule from '@lukso/web3-onboard-config';
-import injectedModule from '@web3-onboard/injected-wallets';
+import React, { useState } from 'react';
+import Web3 from 'web3';
 
-const WalletConnector = () => {
-  const [onboard, setOnboard] = useState(null);
-  const [connectedWallets, setConnectedWallets] = useState([]);
+declare global {
+  interface Window {
+    lukso: any;
+  }
+}
 
-  useEffect(() => {
-    const initializeOnboard = async () => {
-      const lukso = luksoModule();
+const WalletConnector: React.FC = () => {
+  const [account, setAccount] = useState<string | null>(null);
 
-      const injected = injectedModule({
-        custom: [lukso],
-        sort: (wallets) => {
-          const sorted = wallets.reduce((sorted, wallet) => {
-            if (wallet.label === 'Universal Profiles') {
-              sorted.unshift(wallet);
-            } else {
-              sorted.push(wallet);
-            }
-            return sorted;
-          }, []);
-          return sorted;
-        },
-        displayUnavailable: ['Universal Profiles'],
-      });
+  // Check if the Universal Profile Extension  is installed
+  if (!window.lukso) {
+    return <div>Please install the Universale Profile Extension to use this app.</div>;
+  }
 
-      const chains = [
-        {
-          id: 1,
-          token: 'LYX',
-          label: 'LUKSO Mainnet',
-          rpcUrl: 'https://rpc.lukso.network',
-        },
-        {
-          id: 22, // L14 Testnet chainId is 22
-          token: 'LYXt',
-          label: 'LUKSO L14 Testnet',
-          rpcUrl: 'https://rpc.l14.lukso.network',
-        },
-      ];
+  // Create a Web3 instance
+  const web3 = new Web3(window.lukso);
 
-      const appMetadata = {
-        name: 'LUKSO Test dApp',
-        icon: '<svg></svg>', // Replace with actual SVG icon
-        logo: '<svg></svg>', // Replace with actual SVG icon
-        description: 'My test dApp using Web3 Onboard',
-        recommendedInjectedWallets: [
-          {
-            name: 'Universal Profiles',
-            url: "https://chrome.google.com/webstore/detail/universal-profiles/abpickdkkbnbcoepogfhkhennhfhehfn?hl=en",
-        },
-        ],
-      };
-
-      const connect = {
-        iDontHaveAWalletLink: "https://chrome.google.com/webstore/detail/universal-profiles/abpickdkkbnbcoepogfhkhennhfhehfn?hl=en",
-        removeWhereIsMyWalletWarning: true,
-      };
-
-      const onboardInstance = Onboard({
-        wallets: [injected],
-        chains,
-        appMetadata,
-        connect,
-      });
-
-      setOnboard(onboardInstance);
-    };
-
-    initializeOnboard();
-  }, []);
-
-  const connectWallet = async () => {
-    const wallets = await onboard.connectWallet();
-    setConnectedWallets(wallets);
-    console.log(wallets);
-  };
+  async function connect() {
+    try {
+      const accounts = await web3.eth.requestAccounts();
+      console.log('Connected with', accounts[0]);
+      setAccount(accounts[0]); // Update state with the connected account
+    } catch (error) {
+      console.error('Connection error', error);
+    }
+  }
 
   return (
     <div>
-      <button onClick={connectWallet}>Connect Wallet</button>
-      {connectedWallets.length > 0 && (
-        <div>
-          <p>Connected Wallets:</p>
-          <ul>
-            {connectedWallets.map((wallet, index) => (
-              <li key={index}>{wallet.label} - {wallet.accounts[0].address}</li>
-            ))}
-          </ul>
-        </div>
+      {account ? (
+        <p>Connected with: {account}</p>
+      ) : (
+        <button onClick={connect}>Connect Wallet</button>
       )}
     </div>
   );
