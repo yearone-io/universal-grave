72a2e50cca83af2fc919161d53211991d97a152d
Ledespana <no-reply@ledespana.com>
2023-11-09 12:44:53 +0100

    store wallet

--- frontend/components/WalletConnector.tsx
+++ frontend/components/WalletConnector.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Web3 from 'web3';
 
 declare global {
@@ -7,31 +7,52 @@ declare global {
   }
 }
 
+
 const WalletConnector: React.FC = () => {
   const [account, setAccount] = useState<string | null>(null);
 
-  // Check if the Universal Profile Extension  is installed
-  if (!window.lukso) {
-    return <div>Please install the Universale Profile Extension to use this app.</div>;
-  }
-
-  // Create a Web3 instance
-  const web3 = new Web3(window.lukso);
-
-  async function connect() {
-    try {
-      const accounts = await web3.eth.requestAccounts();
-      console.log('Connected with', accounts[0]);
-      setAccount(accounts[0]); // Update state with the connected account
-    } catch (error) {
-      console.error('Connection error', error);
+  useEffect(() => {
+    // Check if we are in a browser environment and the lukso wallet provider exists
+    if (typeof window !== 'undefined' && window.lukso) {
+      const storedAccount = localStorage.getItem('connectedAccount');
+      if (storedAccount) {
+        setAccount(storedAccount);
+      }
     }
-  }
+  }, []);
+
+  const connect = async () => {
+    if (typeof window !== 'undefined' && window.lukso) {
+      const web3 = new Web3(window.lukso);
+
+      try {
+        const accounts = await web3.eth.requestAccounts();
+        console.log('Connected with', accounts[0]);
+        setAccount(accounts[0]);
+        localStorage.setItem('connectedAccount', accounts[0]);
+      } catch (error) {
+        console.error('Connection error', error);
+      }
+    } else {
+      console.log('Please install the LUKSO Universal Profile Extension to use this app.');
+    }
+  };
+
+  const disconnect = () => {
+    setAccount(null);
+    localStorage.removeItem('connectedAccount');
+    // Note: If the LUKSO wallet provider has a method for disconnection, call it here.
+    // For example: window.lukso.disconnect(); (if this method is available)
+    // call any other cleanup methods here
+  };
 
   return (
     <div>
       {account ? (
-        <p>Connected with: {account}</p>
+        <>
+          <p>Connected with: {account}</p>
+          <button onClick={disconnect}>Disconnect Wallet</button>
+        </>
       ) : (
         <button onClick={connect}>Connect Wallet</button>
       )}
