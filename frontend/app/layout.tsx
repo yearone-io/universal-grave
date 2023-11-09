b30dd08ddf78ba64795b086f5ede4ec691cf0219
Ledespana <no-reply@ledespana.com>
2023-11-09 20:09:27 +0100

    final

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,8 +2,6 @@
 import Navbar from "@/components/instructionsComponent/navigation/navbar";
 import Footer from "@/components/instructionsComponent/navigation/footer";
 import { WalletProvider } from "@/components/wallet/WalletProvider";
-import { ChakraProvider } from '@chakra-ui/react'
-
 
 export default function RootLayout({
   children,
@@ -13,17 +11,15 @@ export default function RootLayout({
 
   return (
     <html lang="en">    
-      <ChakraProvider>
-          <WalletProvider>
-            <body>
-              <div style={{ display: "flex", flexDirection: "column", minHeight: "105vh" }}>
-                <Navbar />
-                <div style={{flexGrow: 1}}>{children}</div>
-                <Footer />
-              </div>
-            </body>
-          </WalletProvider>
-      </ChakraProvider>
+      <WalletProvider>
+        <body>
+          <div style={{ display: "flex", flexDirection: "column", minHeight: "105vh" }}>
+            <Navbar />
+            <div style={{flexGrow: 1}}>{children}</div>
+            <Footer />
+          </div>
+        </body>
+      </WalletProvider>
       </html>
   );
 }
