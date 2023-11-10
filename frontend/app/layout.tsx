d3c9d57eb70f600e90ef8c7e3431930efb14da91
Ledespana <no-reply@ledespana.com>
2023-11-10 16:58:14 +0100

    layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,23 +1,49 @@
 "use client";
-import Navbar from "@/components/instructionsComponent/navigation/navbar";
-import Footer from "@/components/instructionsComponent/navigation/footer";
+
 import { WalletProvider } from "@/components/wallet/WalletProvider";
+import {
+  ChakraProvider
+} from "@chakra-ui/react";
+import theme from "./theme"
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Head from "next/head";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  let title = "Universal Grave";
 
   return (
     <html lang="en">    
       <WalletProvider>
+      <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          />
+          <title>{title}</title>
+          <meta property="og:title" content={title} />
+          <meta property="og:site_name" content={title} />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:image"
+            content={`${process.env.NEXT_PUBLIC_SITE_URL}/images/nl_square.png`}
+          />
+          <meta name="twitter:card" content="summary"></meta>
+        </Head>
         <body>
+        <ChakraProvider theme={theme} >
           <div style={{ display: "flex", flexDirection: "column", minHeight: "105vh" }}>
+            <Header />
             <div style={{flexGrow: 1}}>{children}</div>
+            <Footer />
           </div>
-        </body>
+          </ChakraProvider>
+          </body>
       </WalletProvider>
-      </html>
+    </html>
   );
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,8 @@
 'use client'
 import "./globals.css";
-import {
-  ChakraProvider
-} from "@chakra-ui/react";
-import theme from "./theme"
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 
 export default function Home() {
-
   return (
-    <ChakraProvider theme={theme} >
-        <Header />
-        <Footer />
-    </ChakraProvider>
+    <div>LANDING PAGE</div>
   );
 }
