8d1e89fd2c9162fb6edff5f2dfe16103783525f0
user.name <user@email>
2023-11-13 09:31:59 +0200

    logos and default theming

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,23 @@
 'use client'
+import { Container, Flex, Stack, Image, Box } from "@chakra-ui/react";
 import "./globals.css";
 
 export default function Home() {
+  const logoPath = '/images/logo-full.png'
   return (
-    <div>LANDING PAGE</div>
+    <Container
+        as={Stack}
+        maxW={"6xl"}
+        py={5}
+        direction={{ base: "column", md: "row" }}
+        spacing={4}
+        justify={{ base: "center", md: "space-between" }}
+        align={{ base: "center", md: "center" }}
+      >
+        <Flex gap="7px" justifyContent="center" alignItems="center"
+        width={"100%"}>
+          <Image src={logoPath} alt="Universal-Grave-logo" width={'300px'}/>
+        </Flex>
+      </Container>
   );
 }
