9af87ae5783ecb6eed67d2a86f4d1eec732a700d
democrazyfi <democrazyfi@gmail.com>
2023-11-15 10:45:25 +0400

    Added LSP0ERC725Account

---
+++ solidity/contracts/MyAccount.sol
@@ -0,0 +1,5 @@
+import "@lukso/lsp-smart-contracts/contracts/LSP0ERC725Account/LSP0ERC725Account.sol";
+
+contract MyAccount is LSP0ERC725Account {
+    constructor(address _newOwner) LSP0ERC725Account(_newOwner) {}
+}
