856a2a6a42b7ed98665c56eee1b697aa37c5da45
Ledespana <no-reply@ledespana.com>
2023-11-25 17:52:26 +0100

    added link

--- frontend/components/joinGravePannel.tsx
+++ frontend/components/joinGravePannel.tsx
@@ -17,40 +17,42 @@ import {
 } from '@chakra-ui/react';
 import JoinGraveBtn from './JoinGraveBtn';
 import { FaCheckCircle } from 'react-icons/fa';
+import { constants } from '@/app/constants';
 
 const initialSteps = [
   {
     title: 'Give your 🆙 necessary permissions',
     instructions: 'Give permissions to your Browser Extension Controller.',
     instructions2: 'This can also be done manually.',
-    completeText: 'PERMISSION SET',
+    completeText: { text: 'PERMISSION SET', address: null },
     complete: false,
   },
   {
     title: 'Create your GRAVE spam box',
-    completeText: 'ADDRESS: ',
+    completeText: { text: 'ADDRESS: ', address: null },
     complete: false,
   },
   {
     title: 'Link GRAVE to your 🆙',
-    completeText: `GRAVE LINKED`,
+    completeText: { text: 'GRAVE LINKED', address: null },
     complete: false,
   },
   {
     title: 'Enable GRAVE to keep assets inventory',
-    completeText: `INVENTORY TRACKED`,
+    completeText: { text: 'INVENTORY TRACKED', address: null },
     complete: false,
   },
   {
     title: 'Direct all 🆙 spam to the GRAVE',
-    completeText: `SPAM IS DEAD`,
+    completeText: { text: 'SPAM IS DEAD', address: null },
     complete: false,
   },
 ];
 
 const JoinGravePannel: React.FC = () => {
   const bgColor = useColorModeValue('light.green.brand', 'dark.purple.200');
-  const [steps, setSteps] = React.useState([...initialSteps]);
+  const [steps, setSteps] = React.useState([...initialSteps])
+
 
   const displayTruncatedAddress = (address: string) => {
     return `${address.substring(0, 5)}...${address.substring(
@@ -76,7 +78,7 @@ const JoinGravePannel: React.FC = () => {
       console.error('Invalid step');
       return;
     }
-
+  
     // Special case for step 0
     if (newStep === 0 && data[0] && modifiedSteps[0].instructions2) {
       const address = `(${displayTruncatedAddress(data[0])})`;
@@ -84,7 +86,7 @@ const JoinGravePannel: React.FC = () => {
         modifiedSteps[0].instructions2 += address;
       }
     }
-
+  
     for (let step = 0; step < modifiedSteps.length; step++) {
       if (step < newStep) {
         modifiedSteps[step].complete = true;
@@ -92,9 +94,7 @@ const JoinGravePannel: React.FC = () => {
         // Special handling for step 1
         if (step === 1 && newStep > 1 && data[1]) {
           const address1 = displayTruncatedAddress(data[1]);
-          if (!modifiedSteps[1].completeText.includes(address1)) {
-            modifiedSteps[1].completeText += address1;
-          }
+          modifiedSteps[1].completeText.address = data[1];
         }
       } else {
         modifiedSteps[step].complete = false;
@@ -159,7 +159,12 @@ const JoinGravePannel: React.FC = () => {
               ) : (
                 <StepDescription>
                   <Flex alignItems="center" gap={1}>
-                    {step.completeText}{' '}
+                  {step.completeText.text}
+                  {step.completeText.address && (
+                    <a href={`${constants.LUKSO_TESTNET_EXPLORER}/address/${step.completeText.address}`}  style={{ textDecoration: "underline" }} target='_blank'>
+                      {displayTruncatedAddress(step.completeText.address)}
+                    </a>
+                  )}
                     {step.complete ? <FaCheckCircle /> : <></>}
                   </Flex>
                 </StepDescription>
