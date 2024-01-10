18a75a73886aaf667bb18cd77e8254aed75f45a0
user.name <user@email>
2024-01-10 11:35:18 +0200

    perms using real example of deployed mainnet UP extension controller

--- frontend/app/constants.ts
+++ frontend/app/constants.ts
@@ -16,20 +16,54 @@ export const constants = {
   },
 };
 
+/*
+extacted using https://erc725-inspect.lukso.tech/data-fetcher
+and 🔑  AddressPermissions:Permissions:<address> for the main controller of new UP! extension profile
+  {
+  "CHANGEOWNER": false,
+  "ADDCONTROLLER": true,
+  "EDITPERMISSIONS": true,
+  "ADDEXTENSIONS": false,
+  "CHANGEEXTENSIONS": false,
+  "ADDUNIVERSALRECEIVERDELEGATE": false,
+  "CHANGEUNIVERSALRECEIVERDELEGATE": false,
+  "REENTRANCY": false,
+  "SUPER_TRANSFERVALUE": true,
+  "TRANSFERVALUE": true,
+  "SUPER_CALL": true,
+  "CALL": true,
+  "SUPER_STATICCALL": true,
+  "STATICCALL": true,
+  "SUPER_DELEGATECALL": false,
+  "DELEGATECALL": false,
+  "DEPLOY": true,
+  "SUPER_SETDATA": true,
+  "SETDATA": true,
+  "ENCRYPT": true,
+  "DECRYPT": true,
+  "SIGN": true,
+  "EXECUTE_RELAY_CALL": true
+}
+*/
+
 export const DEFAULT_MAIN_CONTROLLER_PERMISSIONS = 
+  parseInt(PERMISSIONS.SUPER_SETDATA, 16) ^
   parseInt(PERMISSIONS.SETDATA, 16) ^
   parseInt(PERMISSIONS.SIGN, 16) ^
   parseInt(PERMISSIONS.ENCRYPT, 16) ^
   parseInt(PERMISSIONS.DECRYPT, 16) ^
+  parseInt(PERMISSIONS.SUPER_CALL, 16) ^
   parseInt(PERMISSIONS.CALL, 16) ^
+  parseInt(PERMISSIONS.SUPER_STATICCALL, 16) ^
   parseInt(PERMISSIONS.STATICCALL, 16) ^
+  parseInt(PERMISSIONS.SUPER_TRANSFERVALUE, 16) ^
   parseInt(PERMISSIONS.TRANSFERVALUE, 16) ^
+  parseInt(PERMISSIONS.DEPLOY, 16) ^
   parseInt(PERMISSIONS.EXECUTE_RELAY_CALL, 16) ^
   parseInt(PERMISSIONS.EDITPERMISSIONS, 16) ^
   parseInt(PERMISSIONS.ADDCONTROLLER, 16);
 
 export const GRAVE_PERMISSIONS = 
-  parseInt(PERMISSIONS.DEPLOY, 16) ^ // necessary deploying user's Grave Vault
   parseInt(PERMISSIONS.ADDUNIVERSALRECEIVERDELEGATE, 16) ^ // necessary for adding a Grave Forwarder as Universal Receiver Delegate
   parseInt(PERMISSIONS.CHANGEUNIVERSALRECEIVERDELEGATE, 16); // necessary for in future removing a Grave Forwarder as Universal Receiver Delegate
 
