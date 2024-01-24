7553d54ee975969e2c1f3d5e66d397c091a3ec1f
user.name <user@email>
2024-01-24 08:31:36 +0200

    formatting

--- frontend/constants/networks.ts
+++ frontend/constants/networks.ts
@@ -1,16 +1,16 @@
 export const NETWORKS = {
-    mainnet: {
-      chainId: 42,
-      symbol: "LYX",
-      rpcUrl: process.env.NEXT_PUBLIC_MAINNET_RPC_URL,
-      fallbackRpcUrl: "https://rpc.lukso.gateway.fm",
-      explorerURL: "https://explorer.execution.mainnet.lukso.network",
-    },
-    testnet: {
-      chainId: 4201,
-      symbol: "LYXt",
-      rpcUrl: process.env.NEXT_PUBLIC_TESTNET_RPC_URL,
-      fallbackRpcUrl: "https://rpc.testnet.lukso.network",
-      explorerURL: "https://explorer.execution.testnet.lukso.network",
-    },
-  };
+  mainnet: {
+    chainId: 42,
+    symbol: 'LYX',
+    rpcUrl: process.env.NEXT_PUBLIC_MAINNET_RPC_URL,
+    fallbackRpcUrl: 'https://rpc.lukso.gateway.fm',
+    explorerURL: 'https://explorer.execution.mainnet.lukso.network',
+  },
+  testnet: {
+    chainId: 4201,
+    symbol: 'LYXt',
+    rpcUrl: process.env.NEXT_PUBLIC_TESTNET_RPC_URL,
+    fallbackRpcUrl: 'https://rpc.testnet.lukso.network',
+    explorerURL: 'https://explorer.execution.testnet.lukso.network',
+  },
+};
