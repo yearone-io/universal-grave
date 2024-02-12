4b8509badb3691866302920d151aadcd1bd6fb3b
democrazyfi <democrazyfi@gmail.com>
2024-02-12 17:24:11 +0400

    fix fmt

--- frontend/utils/provider.ts
+++ frontend/utils/provider.ts
@@ -5,18 +5,20 @@ const UNKNOWN_LUKSO_NETWORK = '0x2a';
 
 export const getProvider = () => {
   const luksoProvider = getLuksoProvider();
-  if(typeof luksoProvider === 'string') {
+  if (typeof luksoProvider === 'string') {
     return new ethers.providers.JsonRpcProvider(luksoProvider);
   }
   return new ethers.providers.Web3Provider(luksoProvider);
 };
 
 export const getLuksoProvider = () => {
-  const rpcUrl = getNetworkConfig(process.env.NEXT_PUBLIC_DEFAULT_NETWORK!).rpcUrl;
+  const rpcUrl = getNetworkConfig(
+    process.env.NEXT_PUBLIC_DEFAULT_NETWORK!
+  ).rpcUrl;
   if (window.lukso && window.lukso.chainId === UNKNOWN_LUKSO_NETWORK) {
     return rpcUrl;
   }
-  if(window.lukso) {
+  if (window.lukso) {
     return window.lukso;
   }
   return rpcUrl;
