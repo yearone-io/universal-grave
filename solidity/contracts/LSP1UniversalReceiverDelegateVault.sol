5422a0086791cc41f2cdb4dae7aa55432a19c68b
tehnalogos <128236767+tehnalogos@users.noreply.github.com>
2024-02-15 18:45:50 +0200

    YEA-164 Mainnet deployment (#65)
    * mainnet deployment
    
    * fix config
    
    * deployed vault urd
    
    * mainnet graveyards
    
    * your company -> YearOne
    
    * formatting
    
    * remove console logs
    
    ---------
    
    Co-authored-by: user.name <user@email>
    Co-authored-by: Ledespana <12192218+Ledespana@users.noreply.github.com>

---
+++ solidity/contracts/LSP1UniversalReceiverDelegateVault.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+// modules
+import {
+    LSP1UniversalReceiverDelegateVault
+} from "@lukso/lsp-smart-contracts/contracts/LSP1UniversalReceiver/LSP1UniversalReceiverDelegateVault/LSP1UniversalReceiverDelegateVault.sol";
+
+contract LSP1UniversalReceiverDelegateVaultGrave is LSP1UniversalReceiverDelegateVault {
+    constructor() {}
+}
