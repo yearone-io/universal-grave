63341552ed8ab1a7f9774f6439fba6b9c4809894
Ledespana <no-reply@ledespana.com>
2024-04-22 15:07:13 +0200

    initial

--- frontend/components/SendToGravePanel.tsx
+++ frontend/components/SendToGravePanel.tsx
@@ -1,11 +1,174 @@
-import React, { useContext } from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Text,
+  useToast,
+} from '@chakra-ui/react';
+import React, { useContext, useEffect, useState } from 'react';
 import { WalletContext } from '@/components/wallet/WalletContext';
-import { formatAddress } from '@/utils/tokenUtils';
+import { ethers } from 'ethers';
+import LSP1GraveForwarderAbi from '@/abis/LSP1GraveForwarder.json';
+import { LSP1GraveForwarder } from '@/contracts';
+import { BiSolidCheckCircle } from 'react-icons/bi';
 
-const SendToGravePanel = () => {
+const messageState = {
+  isCheckingStatus: 'Checking status...',
+  isAddingToAllowList: 'Adding to allowlist...',
+  isRemovingFromAllowList: 'Removing from allowlist...',
+  tokenAllowListDetected: 'Allowed asset detected',
+  tokenDisallowedDetected: 'Disallowed asset detected',
+  invalidAddress: 'Invalid address',
+  makeSureValidAddress: 'Make sure the address is valid',
+};
+
+export default function ManageAllowList() {
   const walletContext = useContext(WalletContext);
-  const { URDLsp7, networkConfig } = walletContext;
+  const { networkConfig, provider, disconnectIfNetworkChanged } = walletContext;
+  const toast = useToast();
+  const signer = provider.getSigner();
+
+  const graveForwarder = new ethers.Contract(
+    networkConfig.universalGraveForwarder,
+    LSP1GraveForwarderAbi.abi,
+    provider
+  ) as LSP1GraveForwarder;
+
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const [isCheckingStatus, setIsCheckingStatus] = useState<boolean>(false);
+  const [isAddingToAllowList, setIsAddingToAllowList] =
+    useState<boolean>(false);
+  const [isRemovingFromAllowList, setIsRemovingFromAllowList] =
+    useState<boolean>(false);
+  const [tokenAddress, setTokenAddress] = useState<string>('');
+  const [tokenCheckMessage, setTokenCheckMessage] = useState<string>('');
+  const [debouncedTokenAddress, setDebouncedTokenAddress] =
+    useState(tokenAddress);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTokenAddress(event.target.value.toLowerCase());
+  };
+
+  // Effect for debouncing tokenAddress update
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedTokenAddress(tokenAddress);
+    }, 500); // 500ms debounce delay
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [tokenAddress]);
+
+  // Call fetchTokenAllowListStatus using debouncedTokenAddress
+  useEffect(() => {
+    setTokenCheckMessage('');
+    // Ensure there's a value to check and avoid fetching on initial render
+    if (debouncedTokenAddress) {
+      fetchTokenAllowListStatus();
+    }
+  }, [debouncedTokenAddress]);
+
+  const fetchTokenAllowListStatus = async () => {
+    if (!tokenAddress) {
+      return;
+    }
+    setIsSubmitting(true);
+    setIsCheckingStatus(true);
+    graveForwarder
+      .connect(signer)
+      .tokenAllowlist(await signer.getAddress(), tokenAddress.toLowerCase())
+      .then(value => {
+        const message = value
+          ? messageState.tokenAllowListDetected
+          : messageState.tokenDisallowedDetected;
+        setTokenCheckMessage(message);
+      })
+      .catch(reason => {
+        if (reason.message && reason.message.includes('invalid address')) {
+          setTokenCheckMessage(messageState.invalidAddress);
+        } else {
+          setTokenCheckMessage(messageState.makeSureValidAddress);
+        }
+      })
+      .finally(() => {
+        setIsCheckingStatus(false);
+        setIsSubmitting(false);
+      });
+  };
+
+
+  const removeTokenFromAllowList = async () => {
+    setIsSubmitting(true);
+    setIsRemovingFromAllowList(true);
+    return graveForwarder
+      .connect(signer)
+      .removeTokenFromAllowlist(tokenAddress)
+      .then(() => {
+        setTokenCheckMessage('');
+        toast({
+          title: `${tokenAddress} has been removed from allowlist`,
+          status: 'success',
+          position: 'bottom-left',
+          duration: 9000,
+          isClosable: true,
+        });
+      })
+      .catch(reason => {
+        toast({
+          title: `Error removing ${tokenAddress} from allowlist: ${reason.message}`,
+          status: 'error',
+          position: 'bottom-left',
+          duration: 9000,
+          isClosable: true,
+        });
+      })
+      .finally(() => {
+        setIsRemovingFromAllowList(false);
+        setIsSubmitting(false);
+      });
+  };
+
+  const FieldMessage = () => {
+    // Conditional rendering based on the state flags
+    if (isCheckingStatus) {
+      return messageState.isCheckingStatus;
+    }
+
+    if (isRemovingFromAllowList) {
+      return messageState.isRemovingFromAllowList;
+    }
+    // When there is a token check message, show it with the CheckCircleIcon
+    if (tokenCheckMessage) {
+      let icon = <></>;
+      if (
+        tokenCheckMessage === messageState.tokenAllowListDetected ||
+        tokenCheckMessage === messageState.tokenDisallowedDetected
+      ) {
+        icon = (
+          <span
+            style={{
+              display: 'inline-flex',
+              alignItems: 'center',
+              position: 'relative',
+              top: '1px',
+            }}
+          >
+            <BiSolidCheckCircle />
+          </span>
+        );
+      }
+      return (
+        <>
+          {tokenCheckMessage + ' '} {icon}
+        </>
+      );
+    }
+    return null; // Return null if there's no specific message to display
+  };
 
   return (
     <>
@@ -15,23 +178,68 @@ const SendToGravePanel = () => {
         fontFamily="Bungee"
         color="dark.purple.400"
       >
-       Send Tokens to the Grave
+        SEND ASSETS TO YOUR GRAVE
+       </Text>
+      <Text
+        mb={4}
+        mt={4}
+        fontWeight={600}
+        fontFamily="Montserrat"
+        textAlign="start"
+      >
+       BLA BLA BLA
       </Text>
-      <Box mt="20px">
-        <Text>
-          <strong>bla</strong> bla
-        </Text>
-        <a
-          href={`${networkConfig.explorerURL}/address/${URDLsp7}`}
-          style={{ textDecoration: 'underline' }}
-          target="_blank"
+      <FormControl textAlign="start">
+        <FormLabel fontFamily="Bungee" fontWeight={400} fontSize={'14px'}>
+          ASSET ADDRESS
+        </FormLabel>
+        <Box display="flex" alignItems="center" h="50px">
+          <Input
+            autoComplete="off"
+            maxWidth="314px"
+            minWidth="200px"
+            height="25px"
+            fontFamily={'Bungee'}
+            backgroundColor="white"
+            borderColor={'var(--chakra-colors-dark-purple-500)'}
+            _hover={{
+              borderColor: 'var(--chakra-colors-dark-purple-500)',
+            }}
+            _focus={{
+              borderColor: 'var(--chakra-colors-dark-purple-500)',
+              boxShadow: 'none',
+            }}
+            value={tokenAddress}
+            onChange={handleChange}
+            placeholder="PASTE ASSET ADDRESS"
+            _placeholder={{
+              fontWeight: 'bold',
+              color: 'var(--chakra-colors-dark-purple-200)',
+            }}
+          />
+          <Text
+            ml={2}
+            fontFamily="Bungee"
+            fontWeight={400}
+            fontSize={'14px'}
+            maxWidth={'120px'}
+          >
+            {FieldMessage()}
+          </Text>
+        </Box>
+      </FormControl>
+      <Flex justifyContent={'flex-start'} gap={2} width={'100%'}>
+        <Button
+          h={'33px'}
+          mr="10px"
+          isDisabled={isSubmitting}
+          isLoading={isRemovingFromAllowList}
+          onClick={removeTokenFromAllowList}
+          type="submit"
         >
-          {formatAddress(URDLsp7)}
-        </a>
-      </Box>
-
+          POOF!
+        </Button>
+      </Flex>
     </>
   );
-};
-
-export default SendToGravePanel;
+}
